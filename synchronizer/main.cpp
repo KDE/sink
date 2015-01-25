060e022db077f5e3d0a2902433601cbc790f9b86
Christian Mollekopf <chrigi_1@fastmail.fm>
2015-01-25 19:05:33 +0100

    Stop using Akonadi2::Console.
    We need a decent loggin framework.

--- synchronizer/main.cpp
+++ synchronizer/main.cpp
@@ -27,12 +27,10 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
     if (argc < 2) {
-        new Akonadi2::Console(QString("Resource: ???"));
-        Akonadi2::Console::main()->log("Not enough args passed, no resource loaded.");
+        qWarning() << "Not enough args passed, no resource loaded.";
         return app.exec();
     }
 
-    new Akonadi2::Console(QString("Resource: %1").arg(argv[1]));
     Listener *listener = new Listener(argv[1]);
 
     QObject::connect(&app, &QCoreApplication::aboutToQuit,
