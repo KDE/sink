ed0faba01b26579e726f5a2d3cf5efb14140322c
Christian Mollekopf <chrigi_1@fastmail.fm>
2016-05-31 15:44:43 +0200

    ImapMailSyncTest

--- examples/imapresource/tests/imapmailsynctest.cpp
+++ examples/imapresource/tests/imapmailsynctest.cpp
@@ -1,3 +1,21 @@
+/*
+ *   Copyright (C) 2016 Christian Mollekopf <chrigi_1@fastmail.fm>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA.
+ */
 #include <QtTest>
 
 #include <tests/mailsynctest.h>
@@ -64,18 +82,19 @@ protected:
         VERIFYEXEC(imap.remove("INBOX." + folderPath.join('.')));
     }
 
-    void createMessage(const QStringList &folderPath, const QByteArray &message) Q_DECL_OVERRIDE
+    QByteArray createMessage(const QStringList &folderPath, const QByteArray &message) Q_DECL_OVERRIDE
     {
         Imap::ImapServerProxy imap("localhost", 993);
-        VERIFYEXEC(imap.login("doe", "doe"));
-        VERIFYEXEC(imap.append("INBOX." + folderPath.join('.'), message));
+        imap.login("doe", "doe").exec().waitForFinished();
+        imap.append("INBOX." + folderPath.join('.'), message).exec().waitForFinished();
+        return "2:*";
     }
 
     void removeMessage(const QStringList &folderPath, const QByteArray &messages) Q_DECL_OVERRIDE
     {
         Imap::ImapServerProxy imap("localhost", 993);
         VERIFYEXEC(imap.login("doe", "doe"));
-        VERIFYEXEC(imap.remove("INBOX." + folderPath.join('.'), "2:*"));
+        VERIFYEXEC(imap.remove("INBOX." + folderPath.join('.'), messages));
     }
 };
 
--- examples/imapresource/tests/resetmailbox.sh
+++ examples/imapresource/tests/resetmailbox.sh
@@ -4,7 +4,6 @@ sudo echo "sam user.doe.* cyrus c" | cyradm --auth PLAIN -u cyrus -w admin local
 sudo echo "dm user.doe.*" | cyradm --auth PLAIN -u cyrus -w admin localhost
 sudo echo "cm user.doe.test" | cyradm --auth PLAIN -u cyrus -w admin localhost
 sudo echo "sam user.doe cyrus c" | cyradm --auth PLAIN -u cyrus -w admin localhost
-# sudo rm -R /var/spool/imap/d/user/doe/*
-sudo cp /work/source/Sink/tests/data/maildir1/cur/1365777830.R28.localhost.localdomain\:2\,S /var/spool/imap/d/user/doe/test/1.
+sudo cp /work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S /var/spool/imap/d/user/doe/test/1.
 sudo chown cyrus:mail /var/spool/imap/d/user/doe/test/1.
 sudo /usr/lib/cyrus-imapd/reconstruct "user.doe.test"
