c13406530ebd9c07553ab99fb40a070565446a8d
Christian Mollekopf <chrigi_1@fastmail.fm>
2016-12-13 17:50:02 +0100

    Improved debug output.

--- common/bufferadaptor.h
+++ common/bufferadaptor.h
@@ -66,7 +66,7 @@ public:
     virtual QVariant getProperty(const QByteArray &key) const
     {
         if (!mValues.contains(key)) {
-            qWarning() << "Tried to read value that is not avialable; Did you forget to call Query::request?"; 
+            qWarning() << "Tried to read value that is not available; Did you forget to call Query::request? Property: " << key;
             return QVariant{};
         }
         return mValues.value(key);
