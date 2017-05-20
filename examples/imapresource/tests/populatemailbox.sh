cdca3e36eddea7f43787fa6bbc2ad6b669c89185
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-05-20 16:53:53 +0200

    Subscribe to mailboxes in imaptest

--- examples/imapresource/tests/populatemailbox.sh
+++ examples/imapresource/tests/populatemailbox.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 sudo echo "sam user.doe.* cyrus c" | cyradm --auth PLAIN -u cyrus -w admin localhost
+#Delete all mailboxes
 sudo echo "dm user.doe.*" | cyradm --auth PLAIN -u cyrus -w admin localhost
+#Create mailboxes
 sudo echo "cm user.doe.test" | cyradm --auth PLAIN -u cyrus -w admin localhost
 sudo echo "cm user.doe.Drafts" | cyradm --auth PLAIN -u cyrus -w admin localhost
 sudo echo "cm user.doe.Trash" | cyradm --auth PLAIN -u cyrus -w admin localhost
+
+#Set acls so we can create in INBOX
 sudo echo "sam user.doe cyrus c" | cyradm --auth PLAIN -u cyrus -w admin localhost
 
+#Subscribe to mailboxes
+sudo echo "sub INBOX" | cyradm --auth PLAIN -u doe -w doe localhost
+sudo echo "sub INBOX.test" | cyradm --auth PLAIN -u doe -w doe localhost
+sudo echo "sub INBOX.Drafts" | cyradm --auth PLAIN -u doe -w doe localhost
+sudo echo "sub INBOX.Trash" | cyradm --auth PLAIN -u doe -w doe localhost
+
+#Create a bunch of test messages in the test folder
 # for i in `seq 1 5000`;
 # do
 #     # sudo cp /work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S /var/spool/imap/d/user/doe/test/$i.
