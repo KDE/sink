7ec52003ec7f06cbb8897cd70e8d952ead3ca15f
Christian Mollekopf <chrigi_1@fastmail.fm>
2017-11-23 17:15:49 +0100

    Adjust testscript paths to new dockercontainer

--- examples/imapresource/tests/populatemailbox.sh
+++ examples/imapresource/tests/populatemailbox.sh
@@ -20,19 +20,20 @@ sudo echo "sub INBOX.Trash" | cyradm --auth PLAIN -u doe -w doe localhost
 #Create a bunch of test messages in the test folder
 # for i in `seq 1 5000`;
 # do
-#     # sudo cp /work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S /var/spool/imap/d/user/doe/test/$i.
+#     # sudo cp /src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S /var/spool/imap/d/user/doe/test/$i.
 # done
 # Because this is way faster than a loop
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{1..1000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{1001..2000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{2001..3000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{3001..4000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{4001..5000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{5001..6000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{6001..7000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{7001..8000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{8001..9000}.
-sudo tee </work/source/Sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null /var/spool/imap/d/user/doe/test/{9001..10000}.
+FOLDERPATH=/var/spool/imap/u/'user^doe^test'
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{1..1000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{1001..2000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{2001..3000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{3001..4000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{4001..5000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{5001..6000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{6001..7000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{7001..8000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{8001..9000}.
+sudo tee </src/sink/examples/imapresource/tests/data/1365777830.R28.localhost.localdomain\:2\,S >/dev/null $FOLDERPATH/{9001..10000}.
 
-sudo chown -R cyrus:mail /var/spool/imap/d/user/doe/test
-sudo /usr/lib/cyrus-imapd/reconstruct "user.doe.test"
+sudo chown -R cyrus:mail $FOLDERPATH
+sudo reconstruct "user.doe.test"
