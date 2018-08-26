63db3b9bfbea5d79f2dc67d5dff3350e1ce56428
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-08-26 15:02:47 +0200

    resetmailbox script in two commands

--- examples/imapresource/tests/resetmailbox.sh
+++ examples/imapresource/tests/resetmailbox.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo echo "sam user.doe.* cyrus c" | cyradm --auth PLAIN -u cyrus -w admin localhost
-sudo echo "dm user.doe.*" | cyradm --auth PLAIN -u cyrus -w admin localhost
-sudo echo "cm user.doe.test" | cyradm --auth PLAIN -u cyrus -w admin localhost
-sudo echo "subscribe INBOX.test" | cyradm --auth PLAIN -u doe -w doe localhost
-sudo echo "cm user.doe.Drafts" | cyradm --auth PLAIN -u cyrus -w admin localhost
-sudo echo "subscribe INBOX.Drafts" | cyradm --auth PLAIN -u doe -w doe localhost
-sudo echo "cm user.doe.Trash" | cyradm --auth PLAIN -u cyrus -w admin localhost
-sudo echo "subscribe INBOX.Trash" | cyradm --auth PLAIN -u doe -w doe localhost
-sudo echo "sam user.doe cyrus c" | cyradm --auth PLAIN -u cyrus -w admin localhost
+sudo echo "sam user.doe.* cyrus c;
+dm user.doe.*;
+cm user.doe.test;
+cm user.doe.Drafts;
+cm user.doe.Trash;
+sam user.doe cyrus c;
+" | cyradm --auth PLAIN -u cyrus -w admin localhost
+
+sudo echo "sam user.doe.* cyrus c;
+subscribe INBOX.test;
+subscribe INBOX.Drafts;
+subscribe INBOX.Trash;
+" | cyradm --auth PLAIN -u doe -w doe localhost
