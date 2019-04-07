e2b9319e57fb66e6fb50913dc2a461dbd7a0840c
Christian Mollekopf <mollekopf@kolabsys.com>
2019-04-07 13:51:58 +0200

    listen() can directly trigger an acceptConnection call
    ...and at that point we'll require a valid m_checkConnectionsTimer,
    so create that first and then listen.

--- common/listener.cpp
+++ common/listener.cpp
@@ -46,25 +46,13 @@ Listener::Listener(const QByteArray &resourceInstanceIdentifier, const QByteArra
       m_resourceName(resourceType),
       m_resourceInstanceIdentifier(resourceInstanceIdentifier),
       m_clientBufferProcessesTimer(new QTimer(this)),
+      m_checkConnectionsTimer(new QTimer),
       m_messageId(0),
       m_exiting(false)
 {
     connect(m_server.get(), &QLocalServer::newConnection, this, &Listener::acceptConnection);
     SinkTrace() << "Trying to open " << m_resourceInstanceIdentifier;
 
-    if (!m_server->listen(QString::fromLatin1(m_resourceInstanceIdentifier))) {
-        m_server->removeServer(m_resourceInstanceIdentifier);
-        if (!m_server->listen(QString::fromLatin1(m_resourceInstanceIdentifier))) {
-            SinkWarning() << "Utter failure to start server";
-            exit(-1);
-        }
-    }
-
-    if (m_server->isListening()) {
-        SinkTrace() << QString("Listening on %1").arg(m_server->serverName());
-    }
-
-    m_checkConnectionsTimer = std::unique_ptr<QTimer>(new QTimer);
     m_checkConnectionsTimer->setSingleShot(true);
     connect(m_checkConnectionsTimer.get(), &QTimer::timeout, [this]() {
         if (m_connections.isEmpty()) {
@@ -80,6 +68,18 @@ Listener::Listener(const QByteArray &resourceInstanceIdentifier, const QByteArra
     m_clientBufferProcessesTimer->setInterval(0);
     m_clientBufferProcessesTimer->setSingleShot(true);
     connect(m_clientBufferProcessesTimer.get(), &QTimer::timeout, this, &Listener::processClientBuffers);
+
+    if (!m_server->listen(QString::fromLatin1(m_resourceInstanceIdentifier))) {
+        m_server->removeServer(m_resourceInstanceIdentifier);
+        if (!m_server->listen(QString::fromLatin1(m_resourceInstanceIdentifier))) {
+            SinkWarning() << "Utter failure to start server";
+            exit(-1);
+        }
+    }
+
+    if (m_server->isListening()) {
+        SinkTrace() << QString("Listening on %1").arg(m_server->serverName());
+    }
 }
 
 Listener::~Listener()
