90635ff8566453729a45ed127c79cd223dc59c53
Christian Mollekopf <mollekopf@kolabsys.com>
2020-02-28 23:02:36 +0100

    Reproduced the recordRevision failure
    Seems to be failing oddly often with i = 241 (Sometimes also later).

--- tests/storagetest.cpp
+++ tests/storagetest.cpp
@@ -516,6 +516,24 @@ private slots:
         QCOMPARE(result, QByteArray("value2"));
     }
 
+    //FIXME this test currently fails
+    void testRecordRevisionRandom()
+    {
+        Sink::Storage::DataStore store(testDataPath, {dbName, Sink::Storage::DataStore::baseDbs()}, Sink::Storage::DataStore::ReadWrite);
+        auto transaction = store.createTransaction(Sink::Storage::DataStore::ReadWrite);
+
+        for (auto i = 1; i <= 1000; i+=2) {
+            const auto uid = Sink::Storage::DataStore::generateUid();
+            const auto id = Sink::Storage::Identifier::fromDisplayByteArray(uid);
+            qWarning() << uid;
+            Sink::Storage::DataStore::recordRevision(transaction, i, id, "type");
+
+            QCOMPARE(Sink::Storage::DataStore::getTypeFromRevision(transaction, i), QByteArray("type"));
+            QCOMPARE(Sink::Storage::DataStore::getUidFromRevision(transaction, i).toDisplayByteArray(), id.toDisplayByteArray());
+            QCOMPARE(Sink::Storage::DataStore::getLatestRevisionFromUid(transaction, id), i);
+        }
+    }
+
     void setupTestFindRange(Sink::Storage::DataStore::NamedDatabase &db)
     {
         db.write("0002", "value1");
