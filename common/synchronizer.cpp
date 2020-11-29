657b6d3393bd9e964e58e7f2309495322f19259d
Christian Mollekopf <mollekopf@kolabsys.com>
2020-11-29 01:00:32 +0100

    We always run into this on startup, so let's not make it too noisy

--- common/synchronizer.cpp
+++ common/synchronizer.cpp
@@ -601,7 +601,7 @@ void Synchronizer::setBusy(bool busy, const QString &reason, const QByteArray re
 KAsync::Job<void> Synchronizer::processSyncQueue()
 {
     if (secret().isEmpty()) {
-        SinkLogCtx(mLogCtx) << "Secret not available but required.";
+        SinkTraceCtx(mLogCtx) << "Secret not available but required.";
         emitNotification(Notification::Warning, ApplicationDomain::SyncError, "Secret is not available.", {}, {});
         return KAsync::null<void>();
     }
